17ff155b094f4da18978c1b47f55c37228315888
KARAN-SRIVASTAVA <srivastavakaran319@gmail.com>
2024-01-01 16:14:01 +0530

    optimization done

--- js/style.js
+++ js/style.js
@@ -1,16 +1,17 @@
 setInterval(() => {
-    let date = new Date();
-    let hours = date.getHours();
-    let minutes = date.getMinutes();
-    let seconds = date.getSeconds();
+  let date = new Date();
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let seconds = date.getSeconds();
 
-    let hourHandAngle = 30 * hours + minutes / 2 + seconds / 120;
-    let minuteHandAngle = 6 * minutes + seconds/10;
-    let secongHandAngle = 6 * seconds ;
-    let hour = document.getElementById("hour");
-    hour.style.transform = `rotate(${hourHandAngle}deg)`;
-    let minute = document.getElementById("minute");
-    minute.style.transform = `rotate(${minuteHandAngle}deg)`;
-    let second = document.getElementById("second");
-    second.style.transform = `rotate(${secongHandAngle}deg)`;
-}, 1000)
+  let hourHandAngle = 30 * hours + minutes / 2 + seconds / 120;
+  let minuteHandAngle = 6 * minutes + seconds / 10;
+  let secondHandAngle = 6 * seconds;
+  
+  let hour = document.getElementById("hour");
+  hour.style.transform = `rotate(${hourHandAngle}deg)`;
+  let minute = document.getElementById("minute");
+  minute.style.transform = `rotate(${minuteHandAngle}deg)`;
+  let second = document.getElementById("second");
+  second.style.transform = `rotate(${secondHandAngle}deg)`;
+}, 1000);
